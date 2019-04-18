65338904e0c5b339213c4d04c927a91b15f7f890
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2019-04-18 22:24:35 +0530

    Update file handling3.py

--- file handling3.py
+++ file handling3.py
@@ -1,17 +1,18 @@
 #write() method
 #  1. r+ mode
 '''earlier in file " akansha saxena was written '''
-a=open('aak.txt','r+')
-a.write("second love")  #writes something in file in r+ mode it overwrites the characters
+a=open('aak.txt','r+')    #CODE 
+a.write("second Dove")  #writes something in file in r+ mode it overwrites the characters
 a.close()
 
 '''
 output
-second loveena
+second Doveena
 '''
 # 2. w+ mode
 #earlier in file " akansha saxena was written.
 
+#CODE
 b=open('code.txt','w+') # overwrite the complete file
 b.write("coding is future")
 b.close()
